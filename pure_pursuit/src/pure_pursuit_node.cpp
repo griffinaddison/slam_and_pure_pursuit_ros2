e26bf9ac0d674e5784482671c864d6696d346092
griffinaddison <gaddison@seas.upenn.edu>
2023-02-26 12:03:03 -0500

    got marker array w/ sphere, cube list working

--- pure_pursuit/src/pure_pursuit_node.cpp
+++ pure_pursuit/src/pure_pursuit_node.cpp
@@ -9,6 +9,7 @@
 #include "ackermann_msgs/msg/ackermann_drive_stamped.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "visualization_msgs/msg/marker.hpp"
+#include "visualization_msgs/msg/marker_array.hpp"
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Quaternion.h>
@@ -40,7 +41,7 @@ class PurePursuit : public rclcpp::Node
 
 private:
 
-    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr pub_marker;
+    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr pub_marker;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_pose;
     std::vector<std::vector<float>> positions;
 
@@ -51,7 +52,7 @@ public:
     {
         // TODO: create ROS subscribers and publishers
 
-        pub_marker = this->create_publisher<visualization_msgs::msg::Marker>("visualization_marker", 10);
+        pub_marker = this->create_publisher<visualization_msgs::msg::MarkerArray>("marker_array", 10);
         sub_pose = this->create_subscription<nav_msgs::msg::Odometry>("/ego_racecar/odom", 100 , std::bind(&PurePursuit::pose_callback, this, _1));
 
         //read csv and convert to 2d float array
@@ -94,7 +95,6 @@ public:
 
     {
 
-        RCLCPP_INFO(this->get_logger(), "Published Marker");
 
         double car_x = pose_msg->pose.pose.position.x;
         double car_y = pose_msg->pose.pose.position.y;
@@ -103,61 +103,101 @@ public:
 
 
         // MARKERS
-        visualization_msgs::msg::Marker marker1;
-        marker1.header.frame_id = "map";
-        // marker1.header.stamp = ros::Time::now();
-        marker1.type = visualization_msgs::msg::Marker::SPHERE;
-        marker1.action = visualization_msgs::msg::Marker::MODIFY;
-        marker1.pose.position.x = car_x;
-        marker1.pose.position.y = car_y;
-        marker1.pose.position.z = car_z + 1.0;
-        marker1.pose.orientation.x = 0.0;
-        marker1.pose.orientation.y = 0.0;
-        marker1.pose.orientation.z = 0.0;
-        marker1.pose.orientation.w = 1.0;
-        marker1.scale.x = 1.0;
-        marker1.scale.y = 1.0;
-        marker1.scale.z = 1.0;
-        marker1.color.a = 1.0;
-        marker1.color.r = 1.0;
-        marker1.color.g = 0.0;
-        marker1.color.b = 0.0;
+        // visualization_msgs::msg::Marker marker1;
+        // marker1.header.frame_id = "map";
+        // // marker1.header.stamp = ros::Time::now();
+        // marker1.type = visualization_msgs::msg::Marker::SPHERE;
+        // marker1.action = visualization_msgs::msg::Marker::MODIFY;
+        // marker1.pose.position.x = car_x;
+        // marker1.pose.position.y = car_y;
+        // marker1.pose.position.z = car_z + 1.0;
+        // marker1.pose.orientation.x = 0.0;
+        // marker1.pose.orientation.y = 0.0;
+        // marker1.pose.orientation.z = 0.0;
+        // marker1.pose.orientation.w = 1.0;
+        // marker1.scale.x = 1.0;
+        // marker1.scale.y = 1.0;
+        // marker1.scale.z = 1.0;
+        // marker1.color.a = 1.0;
+        // marker1.color.r = 1.0;
+        // marker1.color.g = 0.0;
+        // marker1.color.b = 0.0;
 
-        pub_marker->publish(marker1);
+        // pub_marker->publish(marker1); 
 
 
+        RCLCPP_INFO(this->get_logger(), "before");
 
-        //now we will publish a marker array with marker points for each waypoint
-        visualization_msgs::msg::Marker marker_array;
-        marker_array.header.frame_id = "map";
-        // marker_array.header.stamp = this->now();
-        marker_array.type = visualization_msgs::msg::Marker::SPHERE_LIST;
-        marker_array.action = visualization_msgs::msg::Marker::ADD;
-        marker_array.pose.orientation.w = 1.0;
-        marker_array.scale.x = 0.1;
-        marker_array.scale.y = 0.1;
-        marker_array.scale.z = 0.1;
-        marker_array.color.a = 1.0;
-        marker_array.color.r = 0.0;
-        marker_array.color.g = 1.0;
-        marker_array.color.b = 0.0;
+        //create the top level marker array
+        visualization_msgs::msg::MarkerArray marker_array;
+        marker_array.markers.resize(2);
 
-        //now we will add each waypoint to the marker array
-        for (auto const& pos : positions)
-        {
-            geometry_msgs::msg::Point p;
-            p.x = pos[0];
-            p.y = pos[1];
-            p.z = 0.0;
-            marker_array.points.push_back(p);
-        }
+        // Sphere Marker
+        marker_array.markers[0].header.frame_id = "map";
+        // marker_array.markers[0].header.stamp = node->now();
+        marker_array.markers[0].id = 0;
+        marker_array.markers[0].type = visualization_msgs::msg::Marker::SPHERE;
+        marker_array.markers[0].action = visualization_msgs::msg::Marker::MODIFY;
+        marker_array.markers[0].pose.position.x = 1.0;
+        marker_array.markers[0].pose.position.y = 2.0;
+        marker_array.markers[0].pose.position.z = 1.0;
+        marker_array.markers[0].scale.x = 1.0;
+        marker_array.markers[0].scale.y = 1.0;
+        marker_array.markers[0].scale.z = 1.0;
+        marker_array.markers[0].color.r = 1.0;
+        marker_array.markers[0].color.g = 0.0;
+        marker_array.markers[0].color.b = 0.0;
+        marker_array.markers[0].color.a = 1.0;
 
-        //now we will publish the marker array
+        // CubeList Marker
+        marker_array.markers[1].header.frame_id = "map";
+        // marker_array.markers[1].header.stamp = node->now();
+        marker_array.markers[1].id = 1;
+        marker_array.markers[1].type = visualization_msgs::msg::Marker::CUBE_LIST;
+        marker_array.markers[1].action = visualization_msgs::msg::Marker::ADD;
+        marker_array.markers[1].scale.x = 1.0;
+        marker_array.markers[1].scale.y = 1.0;
+        marker_array.markers[1].scale.z = 1.0;
+        marker_array.markers[1].color.r = 0.0;
+        marker_array.markers[1].color.g = 1.0;
+        marker_array.markers[1].color.b = 0.0;
+        marker_array.markers[1].color.a = 1.0;
+
+
+     
+        geometry_msgs::msg::Point point;
+        point.x = 2.0;
+        point.y = 1.0;
+        point.z = 0.0;
+
+        marker_array.markers[1].points.push_back(point);
+
+        point.x = 2.0;
+        point.y = 2.0;
+        point.z = 0.0;
+
+        marker_array.markers[1].points.push_back(point);
+
+        point.x = 2.0;
+        point.y = 3.0;
+        point.z = 0.0;
+
+        marker_array.markers[1].points.push_back(point);
+
+        // std::shared_ptr<visualization_msgs::msg::MarkerArray> marker_array_ptr = std::make_shared<visualization_msgs::msg::MarkerArray>(marker_array);
+
+
         pub_marker->publish(marker_array);
 
-        //test print some elements of the positions array
-        // RCLCPP_INFO(this->get_logger(), "positions[0][0] = %f", positions[0][0]);
+        RCLCPP_INFO(this->get_logger(), "after");
 
+
+
+    
+
+
+
+      
 
         // RCLCPP_DEBUG(this->get_logger(), "Debug message");
 
