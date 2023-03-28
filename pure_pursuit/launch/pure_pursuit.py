e006765e711ea22838dd6edd220556b2029baa07
adi-us <adi-us95@gmail.com>
2023-03-28 02:10:08 -0400

    Changed v to 6 and wb from 0.2 to 0.4

--- pure_pursuit/launch/pure_pursuit.py
+++ pure_pursuit/launch/pure_pursuit.py
@@ -20,11 +20,11 @@ def generate_launch_description():
         name='pure_pursuit_node',
         parameters=[
             {"lookahead_distance": 1.25}, #levine 1.25
-            {"velocity": 3.0}, #levine 9.0
+            {"velocity": 6.0}, #levine 9.0
             {"speed_lookahead_distance": 2.0}, #levine 2.0
             {"brake_gain": 1.0}, #levine 12.0
             {"visualize": True},
-            {"wheel_base": 0.20},
+            {"wheel_base": 0.4},
         ]
         )
 
