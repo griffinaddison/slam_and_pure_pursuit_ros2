9838e90a1d61e1903a8ef94469c1fef74787edc1
hydrargyrum5799 <hgoel1000p@gmail.com>
2023-03-29 03:45:43 -0400

    added acceleration on straights

--- pure_pursuit/launch/pure_pursuit.py
+++ pure_pursuit/launch/pure_pursuit.py
@@ -19,12 +19,15 @@ def generate_launch_description():
         executable='pure_pursuit_node',
         name='pure_pursuit_node',
         parameters=[
-            {"lookahead_distance": 1.25}, #levine 1.25
-            {"velocity": 6.0}, #levine 9.0
-            {"speed_lookahead_distance": 2.0}, #levine 2.0
-            {"brake_gain": 1.0}, #levine 12.0
+            {"lookahead_distance": 2.25}, #levine 1.25
+            {"velocity": 5.5}, #levine 9.0
+            {"speed_lookahead_distance": 3.25}, #levine 2.0
+            {"brake_gain": 2.4}, #levine 12.0
             {"visualize": True},
-            {"wheel_base": 0.4},
+            {"wheel_base": 0.45},
+            {"acceleration_lookahead_distance": 5.0},
+            {"curvature_thresh": 0.1},
+            {"accel_gain": 5.0}
         ]
         )
 
